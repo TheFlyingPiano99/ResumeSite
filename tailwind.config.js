a148b59adf5507ed9efa30d15ae81fb211c72022
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2023-02-01 22:49:47 +0100

    Custom classes and color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 
 const colors = require('tailwindcss/colors')
 
-const brick = '#D06224'
-const brown = '#AE431E'
-const olive = '#8A8635'
-const beige = '#E9C891'
-
-const bobagreen = '#9FC088'
-const darkbeige  = '#E8C07D'
-
 module.exports = {
   content: [
     "./src/**/*.{html,js}", "./index.html",
@@ -25,20 +17,20 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: bobagreen,
-        lightprimary: beige,
-        darkprimary: olive,
-        complementer: darkbeige,
-        lightcomplementer: olive,
-        bgcolor: colors.gray[200],
-        darkBgColor1: colors.gray[600],
-        lightbgcolor: colors.gray[100],
-        selectcolor: olive,
-        textcolor: colors.gray[600],
-        darktextcolor: colors.gray[800],
-        lighttextcolor: colors.gray[500],
-        invtextcolor: colors.orange[100],
-        slighttextcolor: colors.gray[400]
+        primary: 'var(--color-primary)',
+        lightPrimary: 'var(--color-light-primary)',
+        darkPrimary: 'var(--color-dark-primary)',
+        complementer: 'var(--color-complementer)',
+        lightComplementer: 'var(--color-light-complementer)',
+        darkComplementer: 'var(--color-dark-complementer)',
+        bgColor: colors.gray[200],
+        lightBgColor: colors.gray[100],
+        darkBgColor: colors.gray[600],
+        textColor: colors.gray[600],
+        darkTextColor: colors.gray[800],
+        lightTextColor: colors.gray[500],
+        invTextColor: colors.gray[100],
+        sLightTextColor: colors.gray[400]
       },
       container: {
         center: true,
